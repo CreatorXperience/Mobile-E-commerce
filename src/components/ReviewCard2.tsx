7eb8406c974124530986eda76abda7a998f10c4c
CreatorXperience <allyearmustobey@gmail.com>
2023-05-08 06:42:50 -0700

    improved several component

---
+++ src/components/ReviewCard2.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const SecondReviewCard = ()=> {
+    return (
+        <div className='container w-100 bg-white shadow-md rounded-md py-4'> 
+     
+        </div>
+    )
+}
+
+export default SecondReviewCard

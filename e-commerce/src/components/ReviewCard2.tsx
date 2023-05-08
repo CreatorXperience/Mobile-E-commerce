e2e5ac9654395a18573c0d7cd464c4fdb53a64cd
CreatorXperience <allyearmustobey@gmail.com>
2023-05-08 06:52:31 -0700

    improved the Review Card component

--- e-commerce/src/components/ReviewCard2.tsx
+++ e-commerce/src/components/ReviewCard2.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 
 
-const SecondReviewCard = ()=>  {
+const SecondReviewCard = ()=> {
     return (
         <div className='container w-100 bg-white shadow-md rounded-md py-4'> 
-        <img src="" alt="myImage"/>
+     
         </div>
     )
 }

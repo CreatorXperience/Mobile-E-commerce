c126d2ad919b11d3f067e4f9570a749c672a508e
CreatorXperience <allyearmustobey@gmail.com>
2023-05-08 04:45:15 -0700

    made several changes to some component

--- src/App.tsx
+++ src/App.tsx
@@ -1,57 +1,32 @@
-import React from 'react';
-import Button from './components/button';
-import Footer from './components/footer';
-import Order from './components/Order';
-import DiscountCard from './components/DiscountCard';
-import ProfileCard from './components/ProfileCard';
-import OrderCard from './components/OrderCard';
-import Notification from './components/notificationCard';
+import React from "react";
+import Button from "./components/button";
+import OrderInfo from "./components/OrderInfo";
 
 
-import '../src/style.css'
-export default class App extends React.Component {
+import "../src/style.css";
+export default class App extends React.Component<{}, { lo: number }> {
+  
+
   render() {
-    let obj =  {
-      padding: "4",
-      width: "4/5",
-      content: "Check out",
-      bgcolor: "900"
-    }
-    let obj2 =  {
-      padding: "4",
-      width: "3/5",
-      content: "Check out",
-      bgcolor: "600"
-    }
-    let obj3 =  {
-      padding: "6",
+   
+    let obj3 = {
+      padding: "6px",
       width: "full",
       content: "Check out",
-      bgcolor: "400"
-    }
+      bgcolor: "400",
+    };
 
-    let obj4 =  {
-      Name: 'John Doe', 
-      content:" 25 rue Robert Latouche, Nice, 06200, Côte D’azur, France"
-    }
+    let obj5 = {
+      Order: 30,
+      Delivery: 15,
+      Discount: 0
+    };
 
     return (
       <div className="App">
-       Components
-        <Button size= {obj} /> 
-        <Button size= {obj2} /> 
-        <Button size= {obj3} /> 
-        <Order />
-        <Footer />
-<OrderCard />
-
-<ProfileCard resources={obj4} />
-<Notification />
-<DiscountCard />
+        <Button properties={obj3} />
+      <OrderInfo resources={obj5}  isDiscount = {false} />
       </div>
     );
   }
-  
 }
-
-

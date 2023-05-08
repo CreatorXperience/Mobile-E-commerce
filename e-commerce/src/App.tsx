09f0551f2d99dde8220766bf8f9796c22ed17e41
CreatorXperience <allyearmustobey@gmail.com>
2023-05-08 06:38:39 -0700

    improved the Review Card component

--- e-commerce/src/App.tsx
+++ e-commerce/src/App.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import Button from "./components/button";
 import OrderInfo from "./components/OrderInfo";
 import ReviewCard from "./components/ReviewCard";
-
 import "../src/style.css";
+import SecondReviewCard from "./components/ReviewCard2";
 export default class App extends React.Component<{}, { lo: number }> {
   
 
@@ -25,9 +25,10 @@ export default class App extends React.Component<{}, { lo: number }> {
 
     let obj6 = {
       product: 'Movies',
+      imageLink: 'https://i.pinimg.com/564x/db/82/fa/db82fa405d01f15b81eb36cfa0c3ad4e.jpg',
       amount: 20,
       date: '20/03/2020',
-      review: 'Nice Furniture with good delivery. The delivery time is very fast. Then products look like exactly the picture in the app. Besides, color is also the same and quality is very good despite very cheap price'
+      review: 'Nice Furniture with good delivery. delivery time is very fast. Then products look like exactly the picture in the app. Besides, color is also the same and quality is very good despite very cheap price'
     };
 
     return (
@@ -35,6 +36,7 @@ export default class App extends React.Component<{}, { lo: number }> {
         <Button properties={obj3} />
       <OrderInfo resources={obj5}  isDiscount = {false} />
       <ReviewCard productInfo= {obj6} />
+      <SecondReviewCard />
       </div>
     );
   }

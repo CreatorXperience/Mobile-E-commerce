1ce01fc5a43dc9841a96319b2a54040136f5c7bd
CreatorXperience <allyearmustobey@gmail.com>
2023-05-10 04:31:48 -0700

    did the switch component

--- e-commerce/src/components/NotificationSetting.tsx
+++ e-commerce/src/components/NotificationSetting.tsx
@@ -2,13 +2,13 @@ import React from 'react'
 import '../../src/style.css'
 const NotificationSetting = ()=> {
 return (
-    <div className='container w-100 shadow-md rounded-md flex bg-white py-4 px-4 my-4'>
+    <div className='container w-100 shadow-md rounded-md flex bg-white py-4 px-4 my-4 justify-between'>
         <div> 
             Sales
             </div>
-            <div> 
-            <input type='radio' />
-            <label className='switch'> </label>
+            <div className='switch2'> 
+            <input id = 'toggle' type='checkbox' className='inp' />
+            <label htmlFor='toggle'className='switch'> </label>
             </div>
     </div>
 )

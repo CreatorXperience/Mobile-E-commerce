74788517eac3ba578cdf839d3de9f7341d177fc5
CreatorXperience <allyearmustobey@gmail.com>
2023-05-10 07:37:40 -0700

    made changes

--- e-commerce/src/components/NotificationSetting.tsx
+++ e-commerce/src/components/NotificationSetting.tsx
@@ -1,16 +1,6 @@
 import React from 'react'
 import '../../src/style.css'
 const NotificationSetting = ()=> {
-
-return (
-    <div className='container w-100 shadow-md rounded-md flex bg-white py-4 px-4 my-4'>
-        <div> 
-            Sales
-            </div>
-            <div> 
-            <input type='radio' />
-            <label className='switch'> </label>
-
 return (
     <div className='container w-100 shadow-md rounded-md flex bg-white py-4 px-4 my-4 justify-between'>
         <div> 

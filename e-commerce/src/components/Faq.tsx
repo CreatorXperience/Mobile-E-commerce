af4bf1c4a21f2654d2d1df9c2d0857536ec227d9
goodness <goodyc474@gmail.com>
2023-05-11 15:04:14 +0100

    changes made on app

--- e-commerce/src/components/Faq.tsx
+++ e-commerce/src/components/Faq.tsx
@@ -6,12 +6,10 @@ export const Faq = () => {
     setToggle((prev) => !prev);
   };
   return (
-    <div>
-      <div className="flex justify-between border-2 w-[30%] p-5 mt-2 shadow-md rounded cursor-pointer">
+    <div className="flex justify-center">
+      <div className="flex justify-between border-2 w-[100%] p-5 mt-2 shadow-md rounded cursor-pointer">
         <div>FAQ</div>
-        <div onClick={handleToggle}>
-          {toggle ? Svg.dropdown2() : Svg.dropdown()}
-        </div>
+        <div onClick={handleToggle}>{toggle ? Svg.back() : Svg.forward()}</div>
       </div>
     </div>
   );
--- e-commerce/src/components/NotificationSetting.tsx
+++ e-commerce/src/components/NotificationSetting.tsx
@@ -1,27 +1,16 @@
-import React from 'react'
-import '../../src/style.css'
-const NotificationSetting = ()=> {
-
-return (
-    <div className='container w-100 shadow-md rounded-md flex bg-white py-4 px-4 my-4'>
-        <div> 
-            Sales
-            </div>
-            <div> 
-            <input type='radio' />
-            <label className='switch'> </label>
-
-return (
-    <div className='container w-100 shadow-md rounded-md flex bg-white py-4 px-4 my-4 justify-between'>
-        <div> 
-            Sales
-            </div>
-            <div className='switch2'> 
-            <input id = 'toggle' type='checkbox' className='inp' />
-            <label htmlFor='toggle'className='switch'> </label>
-
-            </div>
+import React from "react";
+import "../../src/style.css";
+const NotificationSetting = () => {
+  return (
+    <div className="container w-100 shadow-md rounded-md flex bg-white py-4 px-4 my-4 justify-between">
+      <div>Sales</div>
+      <div className="switch2">
+        <input id="toggle" type="checkbox" className="inp" />
+        <label htmlFor="toggle" className="switch">
+          {" "}
+        </label>
+      </div>
     </div>
-)
-}
-export default NotificationSetting
+  );
+};
+export default NotificationSetting;
